dd7c51dea946af196372cadb6e652ee1723a2622
abril <abrilludmilacorrea@gmail.com>
2024-04-18 16:36:08 -0300

    Comentarios
    Agregue comentarios en el código "Controller_Enemy".

--- Assets/Scripts/Controller_Enemy.cs
+++ Assets/Scripts/Controller_Enemy.cs
@@ -2,21 +2,21 @@
 
 public class Controller_Enemy : MonoBehaviour
 {
-    public static float enemyVelocity;
-    private Rigidbody rb;
+    public static float enemyVelocity; // Se instancia una variable para definir la velocidad del enemigo.
+    private Rigidbody rb; // Se instancia una variable de tipo Rigidbody para que el GameObject responda a las fisicas.
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>(); // Se obtiene el componente Rigidbody del GameObject.
     }
 
     void Update()
     {
-        rb.AddForce(new Vector3(-enemyVelocity, 0, 0), ForceMode.Force);
-        OutOfBounds();
+        rb.AddForce(new Vector3(-enemyVelocity, 0, 0), ForceMode.Force); // Se le aplica una fuerza al enemigo.
+        OutOfBounds(); // Llama a la función "OutOfBounds".
     }
 
-    public void OutOfBounds()
+    public void OutOfBounds() // Detecta si el enemigo esta fuera del mapa, si es asi lo destruye.
     {
         if (this.transform.position.x <= -15)
         {
