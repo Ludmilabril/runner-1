d328365f9034fdbd391aaa33190e8739c28466e0
abril <abrilludmilacorrea@gmail.com>
2024-04-18 17:50:50 -0300

    Comentarios
    Agregue comentarios al código "Controller_Hud".

--- Assets/Scripts/Controller_Hud.cs
+++ Assets/Scripts/Controller_Hud.cs
@@ -3,28 +3,28 @@ using UnityEngine.UI;
 
 public class Controller_Hud : MonoBehaviour
 {
-    public static bool gameOver = false;
-    public Text distanceText;
-    public Text gameOverText;
-    private float distance = 0;
+    public static bool gameOver = false; // Variable booleana para determinar si el jugador pierde o no.
+    public Text distanceText; // Variable del tipo Text para representar la distancia del jugador.
+    public Text gameOverText; // Variable del tipo Text que se mostara cuando el jugador pierde.
+    private float distance = 0; // Variable que representa la distancia del jugador.
 
     void Start()
     {
-        gameOver = false;
-        distance = 0;
-        distanceText.text = distance.ToString();
-        gameOverText.gameObject.SetActive(false);
+        gameOver = false; // Se inicializa la variable gameOver en Falso;
+        distance = 0; // Se inicializa la distancia a 0;
+        distanceText.text = distance.ToString(); // Se configura el texto en la interfaz.
+        gameOverText.gameObject.SetActive(false); // Se desactiva el texto de gameOver.
     }
 
     void Update()
     {
-        if (gameOver)
+        if (gameOver) // La funcion se ejecuta si gameOver es verdadero.
         {
-            Time.timeScale = 0;
-            gameOverText.text = "Game Over \n Total Distance: " + distance.ToString();
-            gameOverText.gameObject.SetActive(true);
+            Time.timeScale = 0; // El tiempo se para.
+            gameOverText.text = "Game Over \n Total Distance: " + distance.ToString(); // Se muestra el texto de gameOver y la distancia total recorrida.
+            gameOverText.gameObject.SetActive(true); // Se activa el texto de gameOver.
         }
-        else
+        else // si gameOver es falso la distancia sigue incrementandose.
         {
             distance += Time.deltaTime;
             distanceText.text = distance.ToString();
