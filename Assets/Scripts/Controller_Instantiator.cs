85877f34500e16a4abc3c151721d36e099962ef7
abril <abrilludmilacorrea@gmail.com>
2024-04-20 16:55:50 -0300

    Comentarios
    Comentarios en los códigos "Controller__Instantiator" y "Controller_Player".

--- Assets/Scripts/Controller_Instantiator.cs
+++ Assets/Scripts/Controller_Instantiator.cs
@@ -3,29 +3,29 @@ using UnityEngine;
 
 public class Controller_Instantiator : MonoBehaviour
 {
-    public List<GameObject> enemies;
-    public GameObject instantiatePos;
-    public float respawningTimer;
-    private float time = 0;
+    public List<GameObject> enemies; // Lista que que contiene a los distintos tipos de enemigos.
+    public GameObject instantiatePos; // Indica la posición en donde se instanciaran los enemigos.
+    public float respawningTimer; // Tiempo que determina cuando apareceran los enemigos.
+    private float time = 0; // Determina el tiempo transcurrido para cambiar la velocidad de los enemigos.
 
     void Start()
     {
-        Controller_Enemy.enemyVelocity = 2;
+        Controller_Enemy.enemyVelocity = 2; // Velocidad inicial de los enemigos.
     }
 
     void Update()
     {
-        SpawnEnemies();
+        SpawnEnemies(); 
         ChangeVelocity();
     }
 
-    private void ChangeVelocity()
+    private void ChangeVelocity() // Esta función cambia la velocidad de los enemigos a lo largo de los 45s.
     {
         time += Time.deltaTime;
         Controller_Enemy.enemyVelocity = Mathf.SmoothStep(1f, 15f, time / 45f);
     }
 
-    private void SpawnEnemies()
+    private void SpawnEnemies() // Esta función determina cuando aparecen los enemigos.
     {
         respawningTimer -= Time.deltaTime;
 
--- Assets/Scripts/Controller_Player.cs
+++ Assets/Scripts/Controller_Player.cs
@@ -2,30 +2,30 @@
 
 public class Controller_Player : MonoBehaviour
 {
-    private Rigidbody rb;
-    public float jumpForce = 10;
-    private float initialSize;
-    private int i = 0;
-    private bool floored;
+    private Rigidbody rb; // Permite utilizar las físicas.
+    public float jumpForce = 10; // La fuerza con la que el Jugador saltará.
+    private float initialSize;  // El tamaño inicial del jugador.
+    private int i = 0; 
+    private bool floored; // Permite identificar si el jugador esta en el piso o no.
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        initialSize = rb.transform.localScale.y;
+        rb = GetComponent<Rigidbody>(); // Accede al componente "Rigidbody" del jugador.
+        initialSize = rb.transform.localScale.y; // Se obtiene el tamaño inicial del jugador.
     }
 
     void Update()
     {
-        GetInput();
+        GetInput(); 
     }
 
-    private void GetInput()
+    private void GetInput() // Funcion que permite al jugador agacharse y saltar.
     {
         Jump();
         Duck();
     }
 
-    private void Jump()
+    private void Jump() // Con la letra "W" el jugador salta.
     {
         if (floored)
         {
@@ -36,7 +36,7 @@ public class Controller_Player : MonoBehaviour
         }
     }
 
-    private void Duck()
+    private void Duck() // Con la letra "S" el jugador, si esta en el piso, reducira su tamaño, si no solo bajara.
     {
         if (floored)
         {
@@ -66,7 +66,7 @@ public class Controller_Player : MonoBehaviour
         }
     }
 
-    public void OnCollisionEnter(Collision collision)
+    public void OnCollisionEnter(Collision collision) // Establece las colisiones con los enemigos y si el jugador esta en el piso.
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
@@ -80,7 +80,7 @@ public class Controller_Player : MonoBehaviour
         }
     }
 
-    private void OnCollisionExit(Collision collision)
+    private void OnCollisionExit(Collision collision) // Revisa si el jugador no esta colisionando con el piso.
     {
         if (collision.gameObject.CompareTag("Floor"))
         {
