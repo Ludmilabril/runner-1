3306d2c443e4379e9259b72596ecc2da2579b08e
abril <abrilludmilacorrea@gmail.com>
2024-04-20 17:51:32 -0300

    Parallax.
    Al Perder el Parallax se detiene.

--- Assets/Scripts/Controller_Hud.cs
+++ Assets/Scripts/Controller_Hud.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class Controller_Hud : MonoBehaviour
@@ -7,6 +8,12 @@ public class Controller_Hud : MonoBehaviour
     public Text distanceText; // Variable del tipo Text para representar la distancia del jugador.
     public Text gameOverText; // Variable del tipo Text que se mostara cuando el jugador pierde.
     private float distance = 0; // Variable que representa la distancia del jugador.
+    public Parallax parallax00, parallax01;
+    public Parallax parallax02, parallax03;
+    public Parallax parallax04, parallax05;
+    public Parallax parallax06, parallax07;
+    public Parallax parallax08, parallax09;
+
 
     void Start()
     {
@@ -23,6 +30,20 @@ public class Controller_Hud : MonoBehaviour
             Time.timeScale = 0; // El tiempo se para.
             gameOverText.text = "Game Over \n Total Distance: " + distance.ToString("f0"); // Se muestra el texto de gameOver y la distancia total recorrida.
             gameOverText.gameObject.SetActive(true); // Se activa el texto de gameOver.
+
+            if (parallax00 && parallax01 && parallax02 && parallax03 && parallax04 != null)
+            {
+                parallax00.StopParallax(); // El parallax deja de moverse.
+                parallax01.StopParallax();
+                parallax02.StopParallax();
+                parallax03.StopParallax();
+                parallax04.StopParallax();
+                parallax05.StopParallax();
+                parallax06.StopParallax();
+                parallax07.StopParallax();
+                parallax08.StopParallax();
+                parallax09.StopParallax();
+            }
         }
         else // si gameOver es falso la distancia sigue incrementandose.
         {
--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -5,6 +5,7 @@ public class Parallax : MonoBehaviour
     public GameObject cam; // Cámara de la escena.
     private float length, startPos; // Longitud y posición inicial del objeto en el eje x. 
     public float parallaxEffect; // Efecto de movimiento del objeto. 
+    public bool GameOver = false;
 
     void Start() // Obtener la posición inicial del objeto y la longitud del sprite del objeto.
     {
@@ -14,10 +15,18 @@ public class Parallax : MonoBehaviour
 
     void Update() // Movimiento horizontal del objeto.
     {
-        transform.position = new Vector3(transform.position.x - parallaxEffect, transform.position.y, transform.position.z);
-        if (transform.localPosition.x < -20)
+        if (!GameOver) // El parallax se mueve si el jugador no pierde.
         {
-            transform.localPosition = new Vector3(20, transform.localPosition.y, transform.localPosition.z);
+            transform.position = new Vector3(transform.position.x - parallaxEffect, transform.position.y, transform.position.z);
+            if (transform.localPosition.x < -20)
+            {
+                transform.localPosition = new Vector3(20, transform.localPosition.y, transform.localPosition.z);
+            }
         }
+    }
+
+    public void StopParallax() // si el jugador pierde, el parallax se detiene.
+    {
+        GameOver = true;
     }
 }
